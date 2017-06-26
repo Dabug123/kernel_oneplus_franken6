3074f2747bbf11da492649f468c7c56c784716c3
franciscofranco <franciscofranco.1990@gmail.com>
2017-06-26 09:12:19 +0200

    display: add a simple api to query the display state (on/off) at any.
    Signed-off-by: Dabug123 <clarkbrunswick55@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -20,6 +20,7 @@
 #include <video/mipi_display.h>
 #include <linux/firmware.h>
 
+#include <linux/display_state.h>
 #include "dsi_panel.h"
 #include "dsi_ctrl_hw.h"
 #include <linux/project_info.h>
@@ -50,6 +51,13 @@ enum dsi_dsc_ratio_type {
 	DSC_12BPC_8BPP,
 	DSC_RATIO_TYPE_MAX
 };
+
+bool display_on = true;
+
+bool is_display_on()
+{
+	return display_on;
+}
 
 static u32 dsi_dsc_rc_buf_thresh[] = {0x0e, 0x1c, 0x2a, 0x38, 0x46, 0x54,
 		0x62, 0x69, 0x70, 0x77, 0x79, 0x7b, 0x7d, 0x7e};
@@ -507,6 +515,7 @@ static int dsi_panel_power_on(struct dsi_panel *panel)
             goto error_disable_gpio;
         }
     }
+    display_on = true;
 	goto exit;
 
 error_disable_gpio:
@@ -548,6 +557,7 @@ static int dsi_panel_power_off(struct dsi_panel *panel)
 	if (rc)
 		pr_err("[%s] failed to enable vregs, rc=%d\n", panel->name, rc);
     mdss_dsi_disp_poc_en(panel, 0);
+    display_on = false;
 	return rc;
 }
 static int dsi_panel_tx_cmd_set(struct dsi_panel *panel,

888c0ef6856308bb2a625ae3ab9c6f6358a588b5
Dabug123 <clarkbrunswick55@gmail.com>
2018-09-11 14:21:08 +0200

    release

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ export CROSS_COMPILE_ARM32=${HOME}/android/arm-linux-androideabi-4.9/bin/arm-lin
 DEFCONFIG="flash_defconfig"
 
 # Kernel Details
-VER=".beta6"
+VER=".R1"
 
 # Paths
 KERNEL_DIR=`pwd`
